42392e8def402392a8b1bdd91cb4cbc492df5ec5
positivealligator644 <positivealligator29@gmail.com~>
2026-03-19 11:50:52 +0200

    Changed the if statment in Move::is_valid()

--- move.cpp
+++ move.cpp
@@ -4,9 +4,5 @@ Move::Move(int row, int col) : _row(row), _col(col) {}
 
 bool Move::is_valid()
 {
-    if ((this->_row > BOARD_SIZE || this->_row <= 0) || (this->_col > BOARD_SIZE || this->_col <= 0))
-    {
-        return false;
-    }
-    return true;
+    return !((this->_row > BOARD_SIZE || this->_row < 0) || (this->_col > BOARD_SIZE || this->_col < 0));
 }
